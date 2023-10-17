d31cded78709bfd26487f08867ddb86b5e8f0eae
Nigel Temple <ntemple1@ncstudents.niagaracollege.ca>
2023-10-17 19:57:20 -0400

    added search bar and new js file for the code used

---
+++ assets/js/search-bar.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("userInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("table");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+          } else {
+            tr[i].style.display = "none";
+          }
+        }       
+    } 
+}
+
+/* 
+Code help from: https://www.w3schools.com/howto/howto_js_filter_table.asp
+*/
