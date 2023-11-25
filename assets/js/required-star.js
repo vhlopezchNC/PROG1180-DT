52f320d66a4ec9a92a9f870d3dfb147367dfa875
cspirleanu1 <ispirleanu1@ncstudents.niagaracollege.ca>
2023-11-25 16:25:38 -0500

    help system

--- assets/js/required-star.js
+++ assets/js/required-star.js
@@ -1,33 +1,8 @@
-const firstName = document.getElementById('firstName');
-const lastName = document.getElementById('lastName');
+var checkbox = document.getElementById('enableHelpTips');
+  var helpTips = document.querySelectorAll('.help-tip');
 
-
-function addRedStar(inputElement, starId) {
-  const starSpan = document.getElementById(starId);
-
-
-  if (inputElement.required && inputElement.value.trim() === '') {
-    starSpan.textContent = '*'; 
-  } else {
-    starSpan.textContent = ''; 
-  }
-}
-
-firstName.addEventListener('input', function() {
-  addRedStar(firstName, 'firstNameStar');
-});
-
-lastName.addEventListener('input', function() {
-  addRedStar(lastName, 'lastNameStar');
-});
-
-const form = document.querySelector('#formSubmission');
-form.addEventListener('submit', function(event) {
-
-  addRedStar(firstName, 'firstNameStar');
-  addRedStar(lastName, 'lastNameStar');
-
-  if (firstName.value.trim() === '' || lastName.value.trim() === '') {
-    event.preventDefault(); 
-  }
-});
+  checkbox.addEventListener('change', function () {
+    helpTips.forEach(function (helpTip) {
+      helpTip.style.display = checkbox.checked ? 'inline-block' : 'none';
+    });
+  });
