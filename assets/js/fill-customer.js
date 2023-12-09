57a169fc9d2e6ee0583438f32c047ad3ccab41ff
Nigel Temple <you@example.com>
2023-12-09 18:57:17 -0500

    added fill buttons for customer and equipment

---
+++ assets/js/fill-customer.js
@@ -0,0 +1,18 @@
+function fillCustomerValues() {
+    document.getElementById('firstName').value = 'John';
+    document.getElementById('lastName').value = 'Doe';
+    document.getElementById('txtaddress').value = '123 Simple Street';
+    document.getElementById('txtCity').value = 'Toronto';
+    document.getElementById('province').value = 'ON';
+    document.getElementById('txtPostalCode').value = 'A1A 1A1';
+    document.getElementById('email').value = 'example@example.com';
+    document.getElementById('txtPhone').value = '9051234567';
+  }
+
+  function fillEquipmentValues() {
+    document.getElementById('txtAreaDescription').value = 'Lawnmower H345';
+    document.getElementById('txtModelNumber').value = 'TRS-500';
+    document.getElementById('manufacturer').value = '4';
+    document.getElementById('category').value = '3';
+    document.getElementById('color').value = '2';
+  }
