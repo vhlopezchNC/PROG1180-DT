825ee3fcacbe5d3e6e72dc0eacd49e74e5667cac
Nigel Temple <you@example.com>
2023-12-12 22:12:17 -0500

    added autofill for all admin create pages

--- assets/js/fill-customer.js
+++ assets/js/fill-customer.js
@@ -16,3 +16,34 @@ function fillCustomerValues() {
     document.getElementById('category').value = '3';
     document.getElementById('color').value = '2';
   }
+
+  function fillRepairOrderValues() {
+    document.getElementById('textAreaRepairDescription').value = 'Engine Failure: Unable to Start';
+    document.getElementById('warranty').value = '1';
+  }
+
+  function fillWorkLogValues() {
+    document.getElementById('datetimepicker').value = '2023-12-12T12:00';
+    document.getElementById('endDate').value = '2023-12-14T12:00';
+    document.getElementById('textAreaDescription2').value = 'Fuel System Cleaning';
+    document.getElementById('status').value = '2';
+  }
+
+  function fillWarrantyReportValues() {
+    document.getElementById('start').value = '2023-11-12';
+    document.getElementById('end').value = '2023-12-12';
+    document.getElementById('manufacturer2').value = 'JD';
+    document.getElementById('description').value = 'med';
+  }
+
+  function fillRepairReportValues() {
+    document.getElementById('ordDate').value = '2023-11-12';
+    document.getElementById('status2').value = 'C';
+    document.getElementById('technician').value = 'S';
+    document.getElementById('equipment').value = 'RE';
+  }
+
+  function fillWeeklyReportValues() {
+    document.getElementById('sales').value = 'cw';
+    document.getElementById('repairs').value = 'cwr';
+  }
